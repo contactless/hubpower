ae42617b6344fab659344cd1d8469ed0ee6b8167
Andrew Gryaznov <realgrandrew@gmail.com>
2014-02-16 18:38:40 +0400

    created autotools project

--- src/main.c
+++ src/main.c
@@ -1,6 +1,20 @@
-/* hubpower -- control the power settings for a USB hub
- *
- * To build: gcc -o hubpower hubpower.c
+/* -*- Mode: C; indent-tabs-mode: t; c-basic-offset: 4; tab-width: 4 -*-  */
+/*
+ * main.c
+ * Copyright (C) 2014 Andrew Gryaznov <realgrandrew@gmail.com>
+ * 
+ * hubpower is free software: you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License as published by the
+ * Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * hubpower is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See the GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License along
+ * with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
 #include <stdio.h>
@@ -277,4 +291,5 @@ int main(int argc, char **argv)
     }
     return 0;
 }
+
 
